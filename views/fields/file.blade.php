5cbc9b8fad425c77e17521e9a8aa34a5cbeddd1f
Derek Kaijser <derek@webfox.co.nz>
2019-08-22 15:40:57 +1200

    Add filename in Label

--- views/fields/file.blade.php
+++ views/fields/file.blade.php
@@ -1,5 +1,14 @@
 <div>
     <input type="{{ $type }}" name="{{ $name }}" id="{{ $name }}File" class="inputfile {{ $class ?? '' }}" value="{{ $value }}" {!! $extraAttributes  !!}>
-    <label class="btn-secondary btn-block" for="{{ $name }}File"><i class="zmdi zmdi-upload"></i><span>{{ $placeholder ?? 'Select File...' }}</span></label>
+    <label class="btn-secondary btn-block" for="{{ $name }}File">
+        <i class="zmdi zmdi-upload"></i>
+        @if(isset($model) && $model->hasMedia($name))
+            <span>
+                    {{ $model->firstMedia($name)->basename }} (Click to change)
+             </span>
+        @else
+            <span>{{ $placeholder ?? 'Select File...' }}</span>
+        @endif
+    </label>
     <span class="required"></span>
 </div>
